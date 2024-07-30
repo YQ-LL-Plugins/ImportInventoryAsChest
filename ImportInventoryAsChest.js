1bcdcb0fb1ad40fb17df63dc4b34ac6ff14b3d4d
yqs112358 <yqs112358@outlook.com>
2024-07-30 09:57:52 +0800

    change to chest-wall type and fix bugs

--- ImportInventoryAsChest.js
+++ ImportInventoryAsChest.js
@@ -1,7 +1,24 @@
-var MAXX = 5;
-var MAXZ = 6;
-var MAXY = 250;     //堆叠
+var MAXX = 5;       // width
+var MAXZ = 250;     // depth
+var MAXY = 3;       // height
 var EACH_PLAYER_FILE_COUNT = 5;
+
+// Array Helper
+Array.prototype.removeIf = function (target) {
+	// 需要移除元素的索引集合
+    let indexList = [];
+    for (let i = 0; i < this.length; i++) {
+        if (target(this[i]) === true) {
+            indexList.unshift(i);
+        }
+    }
+    // 倒着删
+    for (let index of indexList) {
+        this.splice(index, 1);
+    }
+    return indexList.length > 0;
+};
+
 
 
 function importInventory(basePlayerName, filesList, totalCount)
@@ -9,18 +26,18 @@ function importInventory(basePlayerName, filesList, totalCount)
     filesList.sort();
 
     let done = false;
-    for(let y=0; y<=MAXY; y++)
+    for(let z=0; z<MAXZ; z++)
     {
         if(done) break;
-        for(let z=0; z<=MAXX; z++)
+        for(let y=MAXY-1; y>=0; y--)
         {
             if(done) break;
-            for(let x=0; x<=MAXZ; x++)
+            for(let x=0; x<MAXX; x++)
             {
-                let currentId = y*MAXX*MAXZ + z*MAXX + x;
+                let currentId = z*MAXX*MAXY + (MAXY-1-y)*MAXX + x;      // 前面已有z层，MAXY-1-Y行，x-1个
                 if(currentId + 1 <= totalCount)
                 {
-                    let currentPlayerFiles = filesList.splice(currentId*EACH_PLAYER_FILE_COUNT, currentId*EACH_PLAYER_FILE_COUNT+5);
+                    let currentPlayerFiles = filesList.slice(currentId*EACH_PLAYER_FILE_COUNT, currentId*EACH_PLAYER_FILE_COUNT+5);
                     let tempList = currentPlayerFiles[0].split("-");
                     tempList.pop();
                     let currentPlayerName = (tempList.length == 1 ? tempList[0] : tempList.join('-'));
@@ -32,12 +49,10 @@ function importInventory(basePlayerName, filesList, totalCount)
                     let enderChestPos = new IntPos(x*3+2, y, z*2+1, 0);
                     let signPos = new IntPos(x*3, y, z*2, 0);
                     logger.info(`Create chest id (${x},${y},${z})`);
-                    mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${mainChestPos.x} ~+${mainChestPos.y} ~+${mainChestPos.z} chest`);
-                    mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${mainChestPos.x+1} ~+${mainChestPos.y} ~+${mainChestPos.z} chest`);
-                    mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${enderChestPos.x} ~+${enderChestPos.y} ~+${enderChestPos.z} chest`);
-                    mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${signPos.x} ~+${signPos.y} ~+${signPos.z} birch_wall_sign ["facing_direction"=2]`);
-                
-                    //TODO: change to 墙面
+                    // mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${mainChestPos.x} ~+${mainChestPos.y} ~+${mainChestPos.z} chest`);
+                    // mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${mainChestPos.x+1} ~+${mainChestPos.y} ~+${mainChestPos.z} chest`);
+                    // mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${enderChestPos.x} ~+${enderChestPos.y} ~+${enderChestPos.z} chest`);
+                    // mc.runcmd(`execute as @p[name=${basePlayerName}] run setblock ~+${signPos.x} ~+${signPos.y} ~+${signPos.z} birch_wall_sign ["facing_direction"=2]`);
                 }
                 else{
                     done = true;
@@ -53,6 +68,7 @@ function main()
     mc.regConsoleCmd("importinv", "import inventories as chest", (args) => {
         logger.info("Import process started.");
         let filesList = File.getFilesList("plugins/ImportInventoryAsChest/saved");
+        filesList.removeIf((fileName)=>{ return !fileName.endsWith(".nbt"); });    // 移除非数据文件
         let totalCount = filesList.length / EACH_PLAYER_FILE_COUNT;
         logger.info(`Total ${totalCount} players to import`);
 
