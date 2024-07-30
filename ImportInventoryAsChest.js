81b7d8e66d2c07f3bdc3a1d4759b2e71ee96dda2
yqs112358 <yqs112358@outlook.com>
2024-07-30 12:36:14 +0800

    add player-pos map export and fix bugs

--- ImportInventoryAsChest.js
+++ ImportInventoryAsChest.js
@@ -3,6 +3,10 @@ var MAXZ = 250;     // depth
 var MAXY = 3;       // height
 var EACH_PLAYER_FILE_COUNT = 5;
 var NBT_SAVED_PATH = "plugins/ImportInventoryAsChest/saved";
+var PLAYER_POS_MAP_OUTPUT = "plugins/ImportInventoryAsChest/player_pos_map.txt"
+var ARMOR_START_SLOT = 0
+var OFFHAND_START_SLOT = 9
+
 
 // Array Helper
 Array.prototype.removeIf = function (target) {
@@ -54,37 +58,88 @@ function importInventory(basePlayerName, filesList, totalCount)
                     logger.info(`Processing player #${currentId}: ${currentPlayerName}`);
 
                     let mainChestPos = new IntPos(playerPos.x+x*4, playerPos.y+y, playerPos.z+z*3+1, 0);
+                    let mainChest2Pos = new IntPos(playerPos.x+x*4+1, playerPos.y+y, playerPos.z+z*3+1, 0);
                     let enderChestPos = new IntPos(playerPos.x+x*4+2, playerPos.y+y, playerPos.z+z*3+1, 0);
                     let signPos = new IntPos(playerPos.x+x*4, playerPos.y+y, playerPos.z+z*3, 0);
 
                     // create chest
                     logger.info(`Create chest id (${x},${MAXY-1-y},${z})`);
                     mc.runcmd(`setblock ${mainChestPos.x} ${mainChestPos.y} ${mainChestPos.z} chest`);
-                    mc.runcmd(`setblock ${mainChestPos.x+1} ${mainChestPos.y} ${mainChestPos.z} chest`);
+                    mc.runcmd(`setblock ${mainChest2Pos.x} ${mainChest2Pos.y} ${mainChest2Pos.z} chest`);
                     mc.runcmd(`setblock ${enderChestPos.x} ${enderChestPos.y} ${enderChestPos.z} chest`);
                     mc.runcmd(`setblock ${signPos.x} ${signPos.y} ${signPos.z} birch_wall_sign ["facing_direction"=2]`);
-                    
+                    File.writeLine(PLAYER_POS_MAP_OUTPUT, `${currentPlayerName}: (${x},${MAXY-1-y},${z})`)      // record to file
+
                     // edit sign
                     let signBlockEntity = mc.getBlock(signPos).getBlockEntity();
                     let signNbt = signBlockEntity.getNbt();
                     signNbt.setString("Text",`\n${currentPlayerName}\n(${x},${MAXY-1-y},${z})`);
                     signBlockEntity.setNbt(signNbt);
 
-                    // import inventory to chest
-                    logger.info(`Importing inventory...`);
-                    let chestBlockEntity = mc.getBlock(mainChestPos).getBlockEntity();
-                    let chestNbt = chestBlockEntity.getNbt();
-                    let nbtFile = NBT_SAVED_PATH + "/" + currentPlayerName + "-Inventory.nbt";
-                    let itemsList = parseBinaryNbtFromFile(nbtFile).getTag("Inventory");
+                    // import armor to chest
+                    logger.info(`Importing armor...`);
+                    let mainChest2BlockEntity = mc.getBlock(mainChest2Pos).getBlockEntity();
+                    let mainChest2Nbt = mainChest2BlockEntity.getNbt();
+                    let nbtFile = NBT_SAVED_PATH + "/" + currentPlayerName + "-Armor.nbt";
+                    let itemsList = parseBinaryNbtFromFile(nbtFile).getTag("Armor");
                     let listSize = itemsList.getSize();
                     for(let index = 0; index < listSize; index++)
                     {
                         let itemCompound = itemsList.getTag(index);
                         if(itemCompound.getData("Count") == 0)
                             continue;       // empty slot
-                        chestNbt.getTag("Items").addTag(itemCompound);
+                        itemCompound.setByte("Slot", ARMOR_START_SLOT + index);
+                        mainChest2Nbt.getTag("Items").addTag(itemCompound);
                     }
-                    chestBlockEntity.setNbt(chestNbt);
+                    mainChest2BlockEntity.setNbt(mainChest2Nbt);
+
+                    // import offhand to chest
+                    logger.info(`Importing offhand...`);
+                    mainChest2Nbt = mainChest2BlockEntity.getNbt();
+                    nbtFile = NBT_SAVED_PATH + "/" + currentPlayerName + "-Offhand.nbt";
+                    itemsList = parseBinaryNbtFromFile(nbtFile).getTag("Offhand");
+                    listSize = itemsList.getSize();
+                    for(let index = 0; index < listSize; index++)
+                    {
+                        let itemCompound = itemsList.getTag(index);
+                        if(itemCompound.getData("Count") == 0)
+                            continue;       // empty slot
+                        itemCompound.setByte("Slot", OFFHAND_START_SLOT + index);
+                        mainChest2Nbt.getTag("Items").addTag(itemCompound);
+                    }
+                    mainChest2BlockEntity.setNbt(mainChest2Nbt);
+
+                    // import inventory to chest
+                    logger.info(`Importing inventory...`);
+                    let mainChestBlockEntity = mc.getBlock(mainChestPos).getBlockEntity();
+                    let mainChestNbt = mainChestBlockEntity.getNbt();
+                    nbtFile = NBT_SAVED_PATH + "/" + currentPlayerName + "-Inventory.nbt";
+                    itemsList = parseBinaryNbtFromFile(nbtFile).getTag("Inventory");
+                    listSize = itemsList.getSize();
+                    for(let index = 0; index < listSize; index++)
+                    {
+                        let itemCompound = itemsList.getTag(index);
+                        if(itemCompound.getData("Count") == 0)
+                            continue;       // empty slot
+                        mainChestNbt.getTag("Items").addTag(itemCompound);
+                    }
+                    mainChestBlockEntity.setNbt(mainChestNbt);
+
+                    // import enderchest to chest
+                    logger.info(`Importing enderchest...`);
+                    let enderChestBlockEntity = mc.getBlock(enderChestPos).getBlockEntity();
+                    let enderChestNbt = enderChestBlockEntity.getNbt();
+                    nbtFile = NBT_SAVED_PATH + "/" + currentPlayerName + "-EndChest.nbt";
+                    itemsList = parseBinaryNbtFromFile(nbtFile).getTag("EnderChestInventory");
+                    listSize = itemsList.getSize();
+                    for(let index = 0; index < listSize; index++)
+                    {
+                        let itemCompound = itemsList.getTag(index);
+                        if(itemCompound.getData("Count") == 0)
+                            continue;       // empty slot
+                        enderChestNbt.getTag("Items").addTag(itemCompound);
+                    }
+                    enderChestBlockEntity.setNbt(enderChestNbt);
                 }
                 else{
                     done = true;
